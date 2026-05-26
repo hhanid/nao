aa2682aed35506a1225ae782ae78adea8519843a
socallmebertille <sarah.bejard@gmail.com>
2026-05-26 16:45:08 +0200

    add favorites and pinned stories in story tab

--- apps/backend/src/db/abstractSchema.ts
+++ apps/backend/src/db/abstractSchema.ts
@@ -115,4 +115,7 @@ export type ScheduledJobStatus = DBScheduledJob['status'];
 export type DBBrandingConfig = typeof sqliteSchema.brandingConfig.$inferSelect;
 export type NewBrandingConfig = typeof sqliteSchema.brandingConfig.$inferInsert;
 
+export type DBStoryFavorite = typeof sqliteSchema.storyFavorite.$inferSelect;
+export type NewStoryFavorite = typeof sqliteSchema.storyFavorite.$inferInsert;
+
 export default allSchema as typeof sqliteSchema;
